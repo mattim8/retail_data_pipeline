6add4ed6f993ff7f201e381288a400d299806d74
mat <matviitm@gmail.com>
2026-01-16 22:34:40 +0300

    add schemas/raw tables

---
+++ postgres/init/02_create_raw_tables.sql
@@ -0,0 +1,41 @@
+\connect retail
+CREATE TABLE IF NOT EXISTS raw.customers (
+    customer_id TEXT,
+    customer_unique_id TEXT,
+    customer_zip_code_prefix TEXT,
+    customer_city TEXT,
+    customer_state TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.orders (
+    order_id TEXT,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TEXT,
+    order_approved_at TEXT,
+    order_delivered_carrier_date TEXT,
+    order_delivered_customer_date TEXT,
+    order_estimated_delivery_date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.products (
+    product_id TEXT,
+    product_category_name TEXT,
+    product_name_lenght TEXT,
+    product_description_lenght TEXT,
+    product_photos_qty TEXT,
+    product_weight_g TEXT,
+    product_length_cm TEXT,
+    product_height_cm TEXT,
+    product_width_cm TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw.order_items (
+    order_id TEXT,
+    order_item_id TEXT,
+    product_id TEXT,
+    seller_id TEXT,
+    shipping_limit_date TEXT,
+    price TEXT,
+    freight_value TEXT
+);
