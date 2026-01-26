2070cefb354ed1490fa41f1fae15babdad7ea691
mat <matviitm@gmail.com>
2026-01-26 15:40:40 +0300

    daily sales data mart

--- sql/marts/mart.daily_sales.sql
+++ sql/marts/mart.daily_sales.sql
@@ -1,21 +1,31 @@
-WITH daily_metrics AS (
+CREATE TABLE IF NOT EXISTS mart.daily_sales (
+    order_date DATE PRIMARY KEY,
+    total_orders INT,
+    total_revenue NUMERIC(18,2),
+    aov NUMERIC(18,2),
+    unique_customers INT
+);
+
+TRUNCATE TABLE mart.daily_sales;
+INSERT INTO mart.daily_sales(order_date, total_orders, total_revenue, aov, unique_customers)
+    WITH daily_metrics AS (
+        SELECT
+            DATE(order_ts) AS order_date,
+            COUNT(DISTINCT order_id) AS total_orders,
+            SUM(price) AS total_revenue,
+            COUNT(DISTINCT customer_id) AS unique_customers,
+            SUM(price) / NULLIF(COUNT(DISTINCT order_id), 0) AS aov
+        FROM core.order_items
+        WHERE status IN ('delivered', 'shipped')
+        GROUP BY 1
+    )
     SELECT
-        DATE(order_ts) AS order_date,
-        COUNT(DISTINCT order_id) AS total_orders,
-        SUM(price) AS total_revenue,
-        COUNT(DISTINCT customer_id) AS unique_customers,
-        SUM(price) / NULLIF(COUNT(DISTINCT order_id), 0) AS aov
-    FROM core.order_items
-    WHERE status IN ('delivered', 'shipped')
-    GROUP BY 1
-)
-SELECT
-    order_date,
-    total_orders,
-    total_revenue,
-    aov,
-    unique_customers
-FROM daily_metrics
-ORDER BY order_date;
+        order_date,
+        total_orders,
+        total_revenue,
+        aov,
+        unique_customers
+    FROM daily_metrics
+    ORDER BY order_date;
 
 
