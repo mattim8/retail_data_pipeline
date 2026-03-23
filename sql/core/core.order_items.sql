d70bf82ef1c82589cc3f087bf1c9155d3c4ce21d
mat <matviitm@gmail.com>
2026-03-23 19:59:45 +0300

    column insert fix

--- sql/core/core.order_items.sql
+++ sql/core/core.order_items.sql
@@ -16,14 +16,14 @@ TRUNCATE TABLE core.order_items;
 INSERT INTO core.order_items(order_id, order_item_id, order_ts, status, customer_id, product_id, price, freight, seller_id)
 SELECT
     o.order_id,
+    oi.order_item_id AS order_item_id,
     o.order_purchase_timestamp::TIMESTAMP AS order_ts,
     o.order_status AS status,
     c.customer_id,
     oi.product_id,
     oi.price::NUMERIC AS price,
     oi.freight_value::NUMERIC AS freight,
-    oi.seller_id,
-    oi.order_item_id AS order_item_id
+    oi.seller_id
 FROM stg.orders o
 JOIN stg.customers c ON o.customer_id = c.customer_id
 JOIN stg.order_items oi ON o.order_id = oi.order_id
